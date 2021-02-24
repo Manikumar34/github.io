3c449540c982c0cdaefa41b5b9f0a0297d16c0a0
Manikumar Galla <manikumarroyal.galla@gmail.com>
2021-02-24 19:39:17 +0530

    1

--- docs/assets/contact.php
+++ docs/assets/contact.php
@@ -1,41 +1,67 @@
 <?php
-/**
- * Contact Section
- */
+
+$errorMSG = "";
+
+// NAME
+if (empty($_POST["name"])) {
+    $errorMSG = "Name is required ";
+} else {
+    $name = $_POST["name"];
+}
+
+// EMAIL
+if (empty($_POST["email"])) {
+    $errorMSG .= "Email is required ";
+} else {
+    $email = $_POST["email"];
+}
+
+// MSG SUBJECT
+if (empty($_POST["msg_subject"])) {
+    $errorMSG .= "Subject is required ";
+} else {
+    $msg_subject = $_POST["msg_subject"];
+}
+
+
+// MESSAGE
+if (empty($_POST["message"])) {
+    $errorMSG .= "Message is required ";
+} else {
+    $message = $_POST["message"];
+}
+
+
+$EmailTo = "email@gmail.com";
+$Subject = "New Message Received";
+
+// prepare email body text
+$Body = "";
+$Body .= "Name: ";
+$Body .= $name;
+$Body .= "\n";
+$Body .= "Email: ";
+$Body .= $email;
+$Body .= "\n";
+$Body .= "Subject: ";
+$Body .= $msg_subject;
+$Body .= "\n";
+$Body .= "Message: ";
+$Body .= $message;
+$Body .= "\n";
+
+// send email
+$success = mail($EmailTo, $Subject, $Body, "From:".$email);
+
+// redirect to success page
+if ($success && $errorMSG == ""){
+   echo "success";
+}else{
+    if($errorMSG == ""){
+        echo "Something went wrong :(";
+    } else {
+        echo $errorMSG;
+    }
+}
+
 ?>
-
-<?php if ( get_theme_mod( 'serenity_lite_contact_section_toggle' ) == '' ) : ?>
-
-<section id="contact" class="contact text-light py-7">
-
-	<div class="container">
-
-		<div class="row">
-
-			<div class="col-sm-12 col-md-8 col-lg-8">
-
-				<?php if ( is_active_sidebar( 'contact-form-widgets' ) ) : ?>
-
-                    <?php dynamic_sidebar( 'contact-form-widgets' ); ?>
-
-                <?php endif; ?>
-
-			</div>
-
-			<div class="col-sm-12 col-md-3 col-lg-3 ml-auto">
-
-				<?php if ( is_active_sidebar( 'contact-info-widgets' ) ) : ?>
-
-                    <?php dynamic_sidebar( 'contact-info-widgets' ); ?>
-
-                <?php endif; ?>
-
-			</div>
-	        
-		</div>
-
-	</div>
-
-</section>
-
-<?php endif; ?>
